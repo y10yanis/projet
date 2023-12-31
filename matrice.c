71151b7e9e52452b08c3a2d35b7043232234f717
y10yanis <154998746+y10yanis@users.noreply.github.com>
2023-12-31 22:29:45 +0100

    Update matrice.c

--- matrice.c
+++ matrice.c
@@ -1,70 +1,158 @@
-//section include..
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
+#define MAX_SIZE 100
+#define BUFFER_SIZE 100
 
+struct Element {
+    int value;
+    int row;
+    int col;
+};
 
-//define
-#define N ... // places dans le buffer
+int B[MAX_SIZE][MAX_SIZE], C[MAX_SIZE][MAX_SIZE];
+int A[MAX_SIZE][MAX_SIZE];
+struct Element T[BUFFER_SIZE];
+int n1, m1, n2, m2;
 
-//variable globales 
-//les matrices
-B,C,A
-//le tampon
-T
-
-//pour la synchronisation 
 pthread_mutex_t mutex;
-sem_t empty;
-sem_t full;
+pthread_cond_t empty;
+pthread_cond_t full;
+int T_index = 0;
 
-// Producteur
-void producer(void)
-{
-  int item;
-//pour chaque ligne 
-//for....
-  {
-    item=produce(item);
-    sem_wait(&empty); // attente d'une place libre
-    pthread_mutex_lock(&mutex);
-     // section critique
-     insert_item();
-    pthread_mutex_unlock(&mutex);
-    sem_post(&full); // il y a une place remplie en plus
-  }
+struct ThreadData {
+    int thread_id;
+};
+
+void remplir(int mat[MAX_SIZE][MAX_SIZE], int rows, int cols) {
+    printf("Entrer les valeurs de la matrice :\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Element [%d][%d] : ", i, j);
+            scanf("%d", &mat[i][j]);
+        }
+    }
 }
 
-void consumer(void)
-{
- int item;
- while(true)
- {
-   sem_wait(&full); // attente d'une place remplie
-   pthread_mutex_lock(&mutex);
-    // section critique
-    item=remove(item);
-   pthread_mutex_unlock(&mutex);
-   sem_post(&empty); // il y a une place libre en plus
- }
+void *producer(void *arg) {
+    struct ThreadData *data = (struct ThreadData *)arg;
+
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < m2; j++) {
+            int result = 0;
+            for (int k = 0; k < m1; k++) {  // Utiliser m1 ici au lieu de m2
+                result += B[i][k] * C[k][j];
+            }
+
+            pthread_mutex_lock(&mutex);
+            while (T_index >= BUFFER_SIZE) {
+                pthread_cond_wait(&empty, &mutex);
+            }
+            T[T_index].value = result;
+            T[T_index].row = i;
+            T[T_index].col = j;
+            T_index++;
+            printf("Producteur %d : Calcul de A[%d][%d] = %d\n", data->thread_id, i, j, result);
+            pthread_cond_signal(&full);
+            pthread_mutex_unlock(&mutex);
+        }
+    }
+
+    pthread_exit(NULL);
 }
 
-int Main ()
-{
-// Initialisation
-sem_init(&mutex,0,1);//exclusion mutuelle 
-sem_init(&empty, 0 , N);  // buffer vide
-sem_init(&full, 0 , 0);   // buffer vide
-//creation des threads
+void *consumer(void *arg) {
+    int consumed = 0;
 
+    while (consumed < n1 * m2) {
+        pthread_mutex_lock(&mutex);
+        while (T_index == 0) {
+            pthread_cond_wait(&full, &mutex);
+        }
+        T_index--;
+        struct Element currentElement = T[T_index];
+        pthread_cond_signal(&empty);
+        pthread_mutex_unlock(&mutex);
 
+        A[currentElement.row][currentElement.col] = currentElement.value;
+        printf("Consommateur : A[%d][%d] = %d\n", currentElement.row, currentElement.col, currentElement.value);
 
-//attente des threads
+        consumed++;
+    }
 
+    pthread_exit(NULL);
+}
 
+int main() {
+    printf("Entrer le nombre de lignes de la matrice B : ");
+    scanf("%d", &n1);
+    printf("Entrer le nombre de colonnes de la matrice B : ");
+    scanf("%d", &m1);
+    printf("Entrer le nombre de lignes de la matrice C : ");
+    scanf("%d", &n2);
+    printf("Entrer le nombre de colonnes de la matrice C : ");
+    scanf("%d", &m2);
 
+    if (m1 != n2) {
+        printf("Les dimensions ne sont pas compatibles, la multiplication est impossible.\n");
+        return 1;
+    } else {
+        remplir(B, n1, m1);
+        remplir(C, m1, m2);  // Utiliser m1 ici au lieu de n2
+        printf("Matrice B : \n");
 
-//destruction...
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < m1; j++) {
+                printf("%d\t", B[i][j]);
+            }
+            printf("\n");
+        }
 
+        printf("Matrice C : \n");
 
+        for (int i = 0; i < m1; i++) {  // Utiliser m1 ici au lieu de n2
+            for (int j = 0; j < m2; j++) {
+                printf("%d\t", C[i][j]);
+            }
+            printf("\n");
+        }
+    }
 
-return 0;
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&empty, NULL);
+    pthread_cond_init(&full, NULL);
+
+    pthread_t threads[n1];
+    pthread_t consumer_thread;
+
+    struct ThreadData thread_data[n1];
+    struct ThreadData consumer_data;
+
+    for (int i = 0; i < n1; i++) {
+        thread_data[i].thread_id = i;
+        pthread_create(&threads[i], NULL, producer, (void *)&thread_data[i]);
+    }
+
+    pthread_create(&consumer_thread, NULL, consumer, NULL);
+
+    for (int i = 0; i < n1; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    pthread_join(consumer_thread, NULL);
+
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&empty);
+    pthread_cond_destroy(&full);
+
+    printf("Le produit : \n");
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < m2; j++) {
+            printf("%d\t", A[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
